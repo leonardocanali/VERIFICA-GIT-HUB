9d2c9faf1d2835a767e5cca0ed583894136c14d6
leonardo.canali <leonardo.canali.stud@ispascalcomandini.it>
2021-11-12 08:44:15 +0100

    AGGIUNTA IMPLEMENTAZIONE COSTRUTTORE

---
+++ Biblioteca.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    class Biblioteca
+    {
+        private string _nome;
+        private string _indirizzo;
+        private string _orarioApertura;
+        private string _orarioChiusura;
+        private List<Libro>_libri;
+
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura)
+        {
+            _nome = nome;
+            _indirizzo = indirizzo;
+            _orarioApertura = orarioApertura;
+            _orarioChiusura = orarioChiusura;
+            _libri = new List<Libro>();
+        }
+
+
+    }
+}
